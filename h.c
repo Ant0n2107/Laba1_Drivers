47447e197ab5931e6fc13915e2df2626b341c2d4
Ant0n2107 <antontertyshnik@mail.ru>
2021-11-20 14:46:12 +0300

    2

--- h.c
+++ h.c
@@ -1,17 +1,26 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+static char *whom = "MAI";
+module_param(whom, charp, 0644);
+MODULE_PARM_DESC(whom, "Recipient of the hello message");
+static int howmany = 1;
+module_param(howmany, int, 0644);
+MODULE_PARM_DESC(howmany, "Number of greetings");
+static char *from = "Ant0n";
+module_param(from, charp, 0644);
+MODULE_PARM_DESC(from, "Who send the hello message");
 static int __init hello_init(void)
 {
-	pr_alert("Privet, MAI\n");
+	int i;
+	for (i = 0; i < howmany; i++)
+		pr_alert("(%d) Hello, %s from %s\n", i, whom, from);
 	return 0;
 }
 static void __exit hello_exit(void)
 {
-	pr_alert("Poka, MAI\n");
+	pr_alert("Goodbye, %s from %s\n", whom, from);
 }
 module_init(hello_init);
 module_exit(hello_exit);
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Lab1");
-MODULE_AUTHOR("Anton");
--- h.mod
+++
@@ -1,2 +0,0 @@
-/home/anton/lab1/h.o
-
